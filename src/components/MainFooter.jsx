5f7bfe8f3c1fb69bd6de77747775849327c38a01
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-16 17:10:51 +0100

    feat: footer nav bar

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,7 +1,66 @@
 const MainFooter = () => {
-    return(
+    return (
         <footer>
-            <h2>Ciao sono il Footer</h2>
+            <img src="" alt="" />
+            <nav className="footer-nav">
+
+                <div className="footer-col">
+                    <div>
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <div>
+                            <h4>SHOP</h4>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className="footer-col">
+                    <div>
+                        <h4>DC</h4>
+                        <ul>
+                            <li><a href="#">Terme of Use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Ad Choises</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent workshop</a></li>
+                            <li><a href="#">Rating</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footer-col">
+                    <div>
+                        <h4>SITES</h4>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </footer>
     )
 }
