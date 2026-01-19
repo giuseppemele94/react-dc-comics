80cc76b1c87117a9bf93cfb6f0fc16f6c0d5d9e9
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-19 15:11:17 +0100

    struttura dati per navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,21 @@
+//struttura dati per navbar
+
+const links = [
+     { id: 1, text: 'CHARACTERS', url: '#', current: false },
+     { id: 2, text: 'COMICS', url: '#', current: true },
+     { id: 3, text: 'MOVIES', url: '#', current: false },
+     { id: 4, text: 'TV', url: '#', current: false },
+     { id: 5, text: 'GAMES', url: '#', current: false },
+     { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+     { id: 7, text: 'VIDEOS', url: '#', current: false },
+     { id: 8, text: 'FANS', url: '#', current: false },
+     { id: 9, text: 'NEWS', url: '#', current: false },
+     { id: 10, text: 'SHOP', url: '#', current: false },
+];
+
+
+
+
 const Navbar = () => {
 
     return (
