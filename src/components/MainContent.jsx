d2751697255e772e2c72c14beb191df120adf38f
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-19 15:55:16 +0100

    creato componente ComicList e generato dinamicamente in pagina

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,17 +1,14 @@
 import MainSectionBar from "./MainSectionBar"
-
+import ComicsList from "./ComicsList"
 
 
 const MainContent = () => {
     return (
         <main>
             <section className="contents">
-                <div class="content-text">
-                    <h2>-- Content goes here --</h2>
-                </div>
             </section>
-
-            <MainSectionBar/>
+            <ComicsList />
+            <MainSectionBar />
         </main>
     )
 }
