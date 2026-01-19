5bbbe711b25a91b4335c7451fe541c13f6d891ff
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-19 15:19:02 +0100

    feat : dinamicizzazione navbar con struttura dati

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,40 @@
-//struttura dati per navbar
-
-const links = [
-     { id: 1, text: 'CHARACTERS', url: '#', current: false },
-     { id: 2, text: 'COMICS', url: '#', current: true },
-     { id: 3, text: 'MOVIES', url: '#', current: false },
-     { id: 4, text: 'TV', url: '#', current: false },
-     { id: 5, text: 'GAMES', url: '#', current: false },
-     { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
-     { id: 7, text: 'VIDEOS', url: '#', current: false },
-     { id: 8, text: 'FANS', url: '#', current: false },
-     { id: 9, text: 'NEWS', url: '#', current: false },
-     { id: 10, text: 'SHOP', url: '#', current: false },
-];
-
-
-
 
 const Navbar = () => {
 
+    //struttura dati per navbar
+
+const links = [
+    { id: 1, text: 'CHARACTERS', url: '#', current: false },
+    { id: 2, text: 'COMICS', url: '#', current: true },
+    { id: 3, text: 'MOVIES', url: '#', current: false },
+    { id: 4, text: 'TV', url: '#', current: false },
+    { id: 5, text: 'GAMES', url: '#', current: false },
+    { id: 6, text: 'COLLECTIBLES', url: '#', current: false },
+    { id: 7, text: 'VIDEOS', url: '#', current: false },
+    { id: 8, text: 'FANS', url: '#', current: false },
+    { id: 9, text: 'NEWS', url: '#', current: false },
+    { id: 10, text: 'SHOP', url: '#', current: false },
+];
+
+
     return (
- <nav>
-                <ul className="nav-list">
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#" className="active">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-            </nav>
-)}
+        <nav>
+            <ul className="nav-list">
+                {
+                    links.map((link) => {
+                        return (
+                            <li key={link.id}>
+                                <a href={link.url} 
+                                className={link.current ? 'active' : ''}>
+                                    {link.text}</a></li>
+                        )
+
+                    })
+                }
+
+            </ul>
+        </nav>
+    )
+}
 
 export default Navbar
