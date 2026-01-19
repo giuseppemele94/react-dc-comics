0e40502869ed5618d7eb69df05c2e8055737d0ba
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-01-19 15:06:54 +0100

    feat : resa navbar come componente

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,23 +1,12 @@
+import Navbar from "./Navbar"
+
 const MainHeader = () => {
     return(
          <header className="header">
             <figure className="logo">
                 <img src="/dc-logo.png" alt="Logo" />
             </figure>
-            <nav>
-                <ul className="nav-list">
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#" className="active">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-            </nav>
+           <Navbar/>
         </header>
     )
 }
